439f4ef0a603cded2cc6f92549188190132b3ffa
hk116 <henokmekonenpl@gmail.com>
2024-12-11 09:05:46 +0100

    Added comments, improve code redeabilty

--- dice_roll.py
+++ dice_roll.py
@@ -1,28 +1,30 @@
 """
-
+This program simulates rolling dice. It asks the user how many dice to roll, rolls that many six-sided dice, 
+and prints the results. The user can type 'exit' to quit the program.
 """
-
 from random import randint
 
 def roll_dice(value: int = 2) -> list[int]:
+    """Rolls the specified number of six-sided dice and returns the results as a list of integers."""
+
     if value <= 0:
         raise ValueError
     
-    rolls: list[int] = []
-    for i in range(value):
-        rolls.append(randint(1, 6))
-        
-    return rolls
+    return [randint(1, 6) for _ in range(value)]
 
 def main():
-    while True:
-        try:
-            user_input: str = input("How many dice do you want to roll? ")
+    """Main loop for user interaction, allowing repeated dice rolls or exiting the program."""
 
-            if user_input.lower() == 'exit':
-                print("Thanks for playing!")
-                break 
-            print(roll_dice(int(user_input)))
+    while True:
+        user_input: str = input("How many dice do you want to roll? ")
+        
+        if user_input.lower() == 'exit':
+            print("Thanks for playing!")
+            break 
+
+        try:
+            num_dice = int(user_input)
+            print(roll_dice(num_dice))
 
         except ValueError:
             print("Please a valid value. or 'exit' to quit.")
