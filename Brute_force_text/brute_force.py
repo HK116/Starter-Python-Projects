cecebf54b809a58520b2fd18e5d5c4f1fb55cf8b
hk116 <henokmekonenpl@gmail.com>
2024-12-16 15:37:53 +0100

    Brute Force password

---
+++ Brute_force_text/brute_force.py
@@ -0,0 +1,57 @@
+"""
+
+"""
+import itertools # for iterables
+import string
+import time
+
+def common_guess(word: str) -> str | None:
+    with open("words.txt", "r") as file:
+        word_list: list[str] = file.read().splitlines()
+
+        for i, match in enumerate(word_list, start=1):
+            if match == word:
+                return f"Common match: {match} (#{i})"
+            
+
+def brute_force(word: str, length: int, has_digits: bool = False, has_symbols: bool = False) -> str | None:
+    chars: str = string.ascii_lowercase
+
+    if has_digits:
+        chars += string.digits
+
+    if has_symbols:
+        chars += string.punctuation
+
+    attempts: int = 0
+    for guess in itertools.product(chars, repeat=length):
+        attempts += 1
+        guess: str = "".join(guess)
+
+        if guess == word:
+            return f"{word} was cracked in {attempts:,} guesses."
+        
+        # print(guess, attempts)
+
+
+def main():
+    print("Searching...")
+    password: str = 'henok'
+
+    start_time: float = time.perf_counter()
+
+    if common_match := common_guess(password):
+        print(common_match)
+    else:
+        if cracked := brute_force(password, length=5, has_digits=False, has_symbols=False):
+            print(cracked)
+        else:
+            print("There was no match...")
+    
+    end_time: float = time.perf_counter()
+    print(round(end_time - start_time, 2), 's')
+
+
+if __name__ == "__main__":
+    main()
+            
